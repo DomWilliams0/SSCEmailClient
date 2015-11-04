e39d92a6e8c7285f34a33822077e0bb51fb2e097
Dom Williams <dwi3774@googlemail.com>
2015-11-04 14:49:19 +0000

    Added ability to change the appearance of the "connecting to mailbox" dialog box

--- src/dxw405/gui/MailGatherer.java
+++ src/dxw405/gui/MailGatherer.java
@@ -6,6 +6,8 @@ import dxw405.util.Logging;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class MailGatherer
 {
@@ -28,8 +30,11 @@ public class MailGatherer
 		ProgressMonitor monitor = new ProgressMonitor(parent, null, null, 0, 100);
 		monitor.setMillisToPopup(0);
 		monitor.setMillisToDecideToPopup(0);
+		monitor.setNote("Working...");
+		monitor.setProgress(0);
 
 		Worker worker = new Worker(monitor);
+		worker.findDialogComponents();
 		worker.execute();
 	}
 
@@ -37,9 +42,14 @@ public class MailGatherer
 	{
 		private ProgressMonitor monitor;
 
+		private JButton cancelButton;
+		private JProgressBar progressBar;
+
 		public Worker(ProgressMonitor monitor)
 		{
 			this.monitor = monitor;
+			this.cancelButton = null;
+			this.progressBar = null;
 		}
 
 		@Override
@@ -47,6 +57,7 @@ public class MailGatherer
 		{
 			if (!mailbox.isConnected())
 			{
+				setIndeterminate(true);
 				monitor.setNote("Connecting to the mailbox...");
 				monitor.setProgress(0);
 
@@ -59,6 +70,8 @@ public class MailGatherer
 				}
 			}
 
+			setIndeterminate(false);
+
 			monitor.setNote("Gathering mail...");
 			try
 			{
@@ -70,6 +83,62 @@ public class MailGatherer
 			}
 
 			return Boolean.TRUE;
+		}
+
+		private void findDialogComponents()
+		{
+			if (cancelButton == null || progressBar == null)
+			{
+				// prevent closing
+				JDialog dialog = (JDialog) monitor.getAccessibleContext().getAccessibleParent();
+				dialog.addWindowListener(new WindowAdapter()
+				{
+					@Override
+					public void windowClosing(WindowEvent e)
+					{
+						EmailClient.halt("Mailbox connection interrupted");
+					}
+				});
+
+				// get cancel button and progress bar
+				recurse(dialog);
+			}
+		}
+
+		private boolean recurse(Container c)
+		{
+			Component[] components = c.getComponents();
+			for (Component child : components)
+			{
+				// complete
+				if (cancelButton != null && progressBar != null)
+					return true;
+
+				// recurse
+				if (child != null && recurse((Container) child))
+					return true;
+
+				// cancel button
+				if (cancelButton == null && child instanceof JButton && ((JButton) child).getText().equals("Cancel"))
+				{
+					cancelButton = (JButton) child;
+				}
+
+				// progress bar
+				else if (progressBar == null && child instanceof JProgressBar)
+				{
+					progressBar = (JProgressBar) child;
+				}
+			}
+
+			return false;
+		}
+
+
+		private void setIndeterminate(boolean indeterminate)
+		{
+			progressBar.setIndeterminate(indeterminate);
+			cancelButton.setVisible(!indeterminate);
 		}
 
 
